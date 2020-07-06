87637708eb05e1707bcc10b935a0129fcb509374
Sabrina Sampaio <47161964+Sabrina-Btt@users.noreply.github.com>
2020-07-06 17:59:57 -0300

    versão otimizada

--- figuras.cpp
+++ figuras.cpp
@@ -20,26 +20,25 @@ int main(){
     };
     Elements<Lines> Le{indices};
 
-    mat4 Model1 = rotate_y(0.2)*rotate_x(0.3)*rotate_z(0.3)*translate(0.3, 0.3, 0.3)*scale(0.45,0.45,0.45);
-    mat4 Model2 = rotate_y(-0.25)*rotate_x(0.60)*translate(-0.40, -2.0, -2.0)*scale(0.15,0.3,0.2);
-    mat4 Model3 = rotate_y(0.8)*rotate_x(0.3)*translate(-0.4, -0.4, -0.4)*scale(0.15,1,0.3);
+    vector<mat4> m = {
+    rotate_y(0.2)*rotate_x(0.3)*rotate_z(0.3)*translate(0.3, 0.3, 0.3)*scale(0.45,0.45,0.45),
+    rotate_y(-0.25)*rotate_x(0.60)*translate(-0.40, -2.0, -2.0)*scale(0.15,0.3,0.2),
+    rotate_y(0.8)*rotate_x(0.3)*translate(-0.4, -0.4, -0.4)*scale(0.15,1,0.3)
+    };
+
+    vector<Color> c = {blue, green, magenta};
     mat4 View = translate(0, 0, -1);
     mat4 Projection = orthogonal(-1, 1, -1, 1, -20, 20);
 
     Graphics3D G{500, 500};
     G.clear();
 
-    SimpleShader::M = Projection*View*Model1;
-    SimpleShader::C = blue;
-    G.draw<SimpleShader::Varying>(V, Le);
+    for(int i=0; i<m.size(); i++){
+        SimpleShader::M = Projection*View*m[i];
+        SimpleShader::C = c[i];
+        G.draw<SimpleShader::Varying>(V, Le);
+    }
 
-    SimpleShader::M = Projection*View*Model2;
-    SimpleShader::C = green;
-    G.draw<SimpleShader::Varying>(V, Le);
-
-    SimpleShader::M = Projection*View*Model3;
-    SimpleShader::C = magenta;
-    G.draw<SimpleShader::Varying>(V, Le);
 
     G.savePNG("output.png");
 
