e6183d87170c86296b956f9012dfaeccd72a7c9c
Sabrina Sampaio <47161964+Sabrina-Btt@users.noreply.github.com>
2020-07-15 12:15:27 -0300

    Versão com recortes em 3D

--- Graphics3D.h
+++ Graphics3D.h
@@ -7,20 +7,44 @@
 #include "Primitives.h"
 #include "rasterization.h"
 #include "matrix.h"
+#include "SimpleShader.h"
 
 class Graphics3D{
 	Image img;
+	std::vector<float> ZBuffer;
 
 	public:
 	Graphics3D(int w, int h) : img{w, h}{}
 
-	void clear(Color col = white){
+	void clear(Color col = black){
 		img.fill(col);
 	}
 
 	void savePNG(const char* filename){
 		img.savePNG(filename);
 	}
+
+	void initZBuffer(){
+		ZBuffer.assign(img.pixels.size(), -1.0f);
+	}
+
+	float& zbuffer(int x, int y){
+		return ZBuffer[y*img.width + x];
+	}
+
+	bool ZBufferTest(int x, int y, float z){
+		if(ZBuffer.empty())
+			return true;
+
+		float& zb = zbuffer(x, y);
+		if(z > zb){
+			zb = z;
+			return true;
+		}
+
+		return false;
+	}
+
 
 	template<class Varying, class Vertex, class Prims>
 	void draw(const std::vector<Vertex>& V, const Prims& p){
@@ -45,11 +69,17 @@ class Graphics3D{
 		if(p.x < 0 || p.y < 0 || p.x >= img.width || p.y >= img.height)
 			return;
 
-		fragmentShader(v, img(p.x,p.y));
+        float z = v.position.z/v.position.w;
+
+		if(ZBufferTest(p.x, p.y, z))
+            fragmentShader(v, img(p.x,p.y));
 	}
 
 	template<class Varying>
 	void drawPrimitive(Line<Varying> line){
+	    if(clip(line))
+            return;
+
 		vec2 v0 = toScreen(line[0].position);
 		vec2 v1 = toScreen(line[1].position);
 
@@ -64,10 +94,123 @@ class Graphics3D{
 		};
 	}
 
+    template<class Varying>
+    bool clip(Line<Varying>& line){
+
+        vec4 n[] = {
+            {0, 0, -1, 1},
+            {0, 0, 1, 1},
+            {1, 0, 0, .9},
+            {-1, 0, 0, .9},
+            {0, 1, 0, .9},
+            {0, -1, 0, .9},
+        };
+
+
+          for(int i=0; i<6; i++){
+
+              float pn = dot(line[0].position,n[i]);
+              float qn = dot(line[1].position,n[i]);
+
+              if (pn < 0 && qn < 0)
+                return true;
+              else if((pn>0 && qn<0)){
+                float t = pn/(pn-qn);
+                vec4 c= lerp(t,line[0].position,line[1].position);
+                line[1].position = c;
+              }
+              else if (pn<0 && qn>0){
+                float t = pn/(pn-qn);
+                vec4 c = lerp(t,line[0].position,line[1].position);
+                line[0].position = c;
+              }
+
+              }
+              return false;
+
+    }
+
 	template<class Varying>
 	void drawPrimitive(Triangle<Varying> tri){
-		// TAREFA
+		if(clip(tri))
+			return;
+
+		vec2 v0 = toScreen(tri[0].position),
+             v1 = toScreen(tri[1].position),
+             v2 = toScreen(tri[2].position);
+
+		std::array<vec2,3> pontos={v0,v1,v2};
+
+        for(Pixel p : rasterizeTriangle(pontos)){
+           vec2 v = {(float)p.x, (float)p.y};
+           std::array<float, 3> b = barycentric(v,pontos);
+           Varying vi = ((b[0]*tri[0])+ (b[1]*tri[1]) + (b[2]*tri[2]));
+           paint(p, vi);
+        }
 	}
+
+	template<class Varying>
+	bool clip(Triangle<Varying>& tri){
+		vec4 n[] = {
+			{0, 0, -1, 1},
+			{0, 0, 1, 1},
+			{1, 0, 0, 1},
+			{-1, 0, 0, 1},
+			{0, 1, 0, 1},
+			{0, -1, 0, 1},
+		};
+		for(int i = 0; i < 6; i++)
+			if(clip(tri, n[i]))
+				return true;
+		return false;
+	}
+
+	template<class Varying>
+	bool clip(Triangle<Varying>& tri, vec4 n){
+	    float pn = dot(tri[0].position,n);
+        float qn = dot(tri[1].position,n);
+        float rn = dot(tri[2].position,n);
+
+        if(pn<0 && qn< 0 && rn<0)
+            return true;
+        else if(pn>0 && qn>0 && rn>0)
+            return false;
+
+        std::vector<Varying> L;
+
+        int j =1;
+
+        for(int i=0; i<3; i++, j++){
+                if(i==2)
+                   j=0;
+
+            if((dot(tri[i].position,n)<0 && dot(tri[j].position,n)>=0) || (dot(tri[i].position,n)>= 0 && dot(tri[j].position,n)<0)){
+                    float t = dot(tri[i].position, n)/(dot(tri[i].position,n)-dot(tri[j].position,n));
+                    Varying aux1;
+                    aux1=(1-t)*tri[i] + t*tri[j];
+                    L.push_back(aux1);
+                }
+
+                if(dot(tri[j].position,n)>=0)
+                    L.push_back(tri[j]);
+
+
+            }
+
+                tri = {L[0], L[1], L[2]};
+                if(L.size()==4)
+                {
+                    Triangle<Varying> ntri = {L[0], L[2], L[3]};
+                    drawPrimitive(ntri);
+                }
+
+        return false;
+
+
+}
+
+
+
 
 
 };
@@ -93,12 +236,39 @@ inline mat4 frustum(float l, float r, float b, float t, float n, float f){
     };
 }
 
+
 inline mat4 perspective(float alpha, float a, float n, float f){
-    float t = fabs(n) * tan((alpha)/2), b = -t;
+    double theta = alpha*M_PI/180.0;
+    float t = fabs(n) * tan((theta)/2), b = -t;
     float r = t*a, l = -r;
 
     return frustum(l,r,b,t,-n,-f);
 }
 
 
+inline mat4 lookAt(vec3 o, vec3 c, vec3 up){
+    vec3 up1 = normalize(up);
+    vec3 f = normalize((c-o));
+    vec3 s = normalize(cross(f,up1));
+    vec3 u = cross(s,f);
+
+    mat4 mv_1 = {
+         s.x,  s.y,  s.z, 0.0,
+         u.x,  u.y,  u.z, 0.0,
+        -f.x, -f.y, -f.z, 0.0,
+         0.0,  0.0,  0.0, 1.0
+    };
+
+    mat4 mv_2 = translate(-o.x,-o.y,-o.z);
+
+    return mv_1 * mv_2;
+
+}
+
+
+
+
+
+
 #endif
+
